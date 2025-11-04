cf2ca3a9a4b54ab9a6fc65c0bd38bbd2d132ee6f
zanderrjohnson-CS <zander.r.johnson@gmail.com>
2025-11-04 09:00:25 -0800

    chap 3 until 3.4.2

--- chatgpt.py
+++ chatgpt.py
@@ -93,3 +93,57 @@ for batch in dataloader:
 
     break
 print(input_embeddings.shape)
+import torch.nn as nn
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout) # New
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1)) # New
+
+    def forward(self, x):
+        b, n_tokens, d_in = x.shape # New batch dimension b
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2) # Changed transpose
+        attn_scores.masked_fill_(  # New, _ ops are in-place
+            self.mask.bool()[:n_tokens, :n_tokens], -torch.inf) 
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights) # New
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalSelfAttention(d_in, d_out, context_length, dropout, qkv_bias) 
+             for _ in range(num_heads)]
+        )
+        self.out_proj = nn.Linear(d_out*num_heads, d_out*num_heads)
+
+    def forward(self, x):
+        context_vec = torch.cat([head(x) for head in self.heads], dim=-1)
+        return self.out_proj(context_vec)
+torch.manual_seed(123)
+
+context_length = max_length
+d_in = output_dim
+
+num_heads=2
+d_out = d_in // num_heads
+
+mha = MultiHeadAttentionWrapper(d_in, d_out, context_length, 0.0, num_heads)
+
+batch = input_embeddings
+context_vecs = mha(batch)
+
+print("context_vecs.shape:", context_vecs.shape)
